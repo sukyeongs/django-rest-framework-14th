84d10c6b47c9f8e6781de9fc6c25fcdc6686edd9
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-07 17:11:32 +0900

    feat(models/posts.py): add posts.py

---
+++ api/models/posts.py
@@ -0,0 +1,18 @@
+from time import timezone
+
+from django.db import models
+
+from api.models.user import User
+
+
+class Post(models.Model):
+    post_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.TextField()
+    numOfLike = models.PositiveIntegerField()
+    post_content = models.TextField()
+    is_comment = models.BooleanField()
+    upload_time = models.DateTimeField(auto_now=True)
+
+    def publish(self):
+        self.upload_time = timezone.now()
+        self.save()
