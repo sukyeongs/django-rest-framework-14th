fc376e61b28003c12f40230b4e7d4980eef2c943
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-07 23:42:41 +0900

    style(images.py): change name of Images to Image

--- api/models/images.py
+++ api/models/images.py
@@ -3,6 +3,6 @@ from django.db import models
 from api.models.posts import Post
 
 
-class Images(models.Model):
+class Image(models.Model):
     post = models.ForeignKey(Post, blank=False, null=False, on_delete=models.CASCADE)
     image_url = models.TextField()
