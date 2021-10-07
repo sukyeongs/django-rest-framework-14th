1c8b4e89d309da4930682497d784ade8eb44f1cf
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-07 17:12:18 +0900

    feat(models/comments.py): add comments.py

---
+++ api/models/comments.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from api.models.posts import Post
+from api.models.user import User
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_content = models.TextField()
+    upload_time = models.DateTimeField(auto_now=True)
