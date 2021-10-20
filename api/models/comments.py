ec52af7f25006945b0ea02585e64debddbff865f
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-21 02:22:08 +0900

    feat(comments.py): update comments.py

--- api/models/comments.py
+++ api/models/comments.py
@@ -5,7 +5,12 @@ from api.models.user import User
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment_post")
+    comment_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_author")
     comment_content = models.TextField()
     upload_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
