27ccfe16871454449d7bcbf10c6358658d44c44f
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-20 11:39:47 +0900

    feat(urls.py): update urls.py

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
