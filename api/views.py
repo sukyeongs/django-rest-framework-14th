8b4f43df44edd1048edba6a9ad2a03cfe6796939
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-21 02:18:33 +0900

    feat(views.py): add GET and POST api method

--- api/views.py
+++ api/views.py
@@ -1,10 +1,23 @@
-from .models import Post
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import *
 from .serializers import PostSerializer
-from rest_framework import viewsets
-from rest_framework import permissions
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@csrf_exempt
+def post_view(request):
+
+    if request.method == 'GET':
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            temp_user = User.objects.get(id=1)
+            serializer.save(post_author=temp_user)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
