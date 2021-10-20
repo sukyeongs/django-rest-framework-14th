bb60b42cce0d949896db45ed6baaa5be97fe4801
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-20 11:40:15 +0900

    feat(views.py): update views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
