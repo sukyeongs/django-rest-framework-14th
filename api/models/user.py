dfb06c5003b5a469f43c3b967116632c7cdbe34f
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-10-21 02:19:09 +0900

    feat(user.py): add UserManager to use AbstractBaseUser

--- api/models/user.py
+++ api/models/user.py
@@ -1,9 +1,17 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
 
 
 class User(AbstractBaseUser):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     USERNAME_FIELD = 'username'
     instagram_id = models.CharField(max_length=100, unique=True)
     is_professional = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    class Meta:
+        managed = True
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
