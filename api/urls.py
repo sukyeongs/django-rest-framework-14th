eb9cf097ca60b6b180b672e1f36b08af6d20f25d
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-11 11:57:19 +0900

    feat(urls.py): add urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import post_view
+from . import views
 
 urlpatterns = [
-    path('posts', post_view)
+    path('posts', views.PostList.as_view()),
+    path('posts/<int:pk>', views.PostDetail.as_view())
 ]
