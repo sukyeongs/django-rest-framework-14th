7b60f892adee9ba12eeb47047db4796e72ed2cf0
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-18 23:47:41 +0900

    feat(urls.py): change urlpatterns to add router

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework import routers
+from .views import PostViewSet
 
-urlpatterns = [
-    path('posts', views.PostList.as_view()),
-    path('posts/<int:pk>', views.PostDetail.as_view())
-]
+# urlpatterns = [
+#     path('posts', views.PostList.as_view()),
+#     path('posts/<int:pk>', views.PostDetail.as_view())
+# ]
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)   # register()함으로써 두 개의 url 생성
+
+urlpatterns = router.urls
