30d480344caa2238a3be153094bcdda445b7a905
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-21 01:02:52 +0900

    feat(models): delete models folder

--- api/models/images.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from api.models.posts import Post
-
-
-class Image(models.Model):
-    post = models.ForeignKey(Post, blank=False, null=False, on_delete=models.CASCADE)
-    image_url = models.TextField()
--- api/models/likes.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from api.models import User, Post
-
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_likes')
