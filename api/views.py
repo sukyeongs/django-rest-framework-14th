b1e902e3b25501d7693a417ec8e3129a671df466
Sukyeong Seo <80563849+sukyeongs@users.noreply.github.com>
2021-11-21 01:05:18 +0900

    feat(views.py): reflect feedback

--- api/views.py
+++ api/views.py
@@ -1,52 +1,20 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 from .models import *
 from .serializers import PostSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from django_filters.rest_framework import filters, FilterSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class PostFilter(FilterSet):
+    post_author = filters.CharFilter(field_name='author')
+
+    class Meta:
+        model = Post
+        fields = ['location',]
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-# class PostList(APIView):
-#     def get(self, request, format=None):
-#         queryset = Post.objects.all()
-#         serializer = PostSerializer(queryset, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request, format=None):
-#         serializer = PostSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-#
-#
-# class PostDetail(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return Post.objects.get(pk=pk)
-#         except Post.DoesNotExist:
-#             raise Http404
-#
-#     def get(self, request, pk):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(post)
-#         return Response(serializer.data)
-#
-#     def put(self, request, pk):
-#         post = self.get_object(pk)
-#         serializer = PostSerializer(post, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     def delete(self, request, pk):
-#         post = self.get_object(pk)
-#         post.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PostFilter
